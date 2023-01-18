4a069a68c538f7e3c73d4c3e70c18d719755ccff
marlisonmourao <bentesmourao@gmail.com>
2023-01-18 16:01:40 -0400

    application header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { Habits } from "./components/Habits";
-
+import { Header } from './components/Header';
 import './styles/global.css';
 
 export function App() {
   return (
-    <div>
-      <Habits completed={3} />
-      <Habits completed={3}/>
-      <Habits completed={4}/>
-      <Habits completed={4}/>
-      <Habits completed={4}/>
-      <Habits completed={4}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+
+        <Header />
+
+      </div>
     </div>
   )
 }
