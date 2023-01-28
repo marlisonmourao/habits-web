a64b3e0808cb3c1b757ef825ef90d95847bcb9a3
marlisonmourao <bentesmourao@gmail.com>
2023-01-28 18:27:59 -0400

    notifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,29 @@ import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
 import './styles/global.css';
 import './lib/dayjs'
+import { api } from './server/axios';
+
+navigator.serviceWorker.register('service-works.js')
+.then(async serviceWork => {
+  let subscription = await serviceWork.pushManager.getSubscription()
+
+  if(!subscription) {
+    const publicKeyResponse = await api.get('/push/public_key')
+
+    subscription = await serviceWork.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: publicKeyResponse.data.publicKey
+    })
+  }
+
+  await api.post('/push/register', {
+    subscription
+  })
+
+  await api.post('/push/send', {
+    subscription
+  })
+})
 
 export function App() {
   return (
